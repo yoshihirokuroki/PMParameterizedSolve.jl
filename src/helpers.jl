b026da27d90128ab0107cdefa8e0083733e1d429
Timothy Knab <knabt@metrumrg.com>
2023-10-26 16:43:20 -0400

    Fixed some stuff.

--- src/helpers.jl
+++ src/helpers.jl
@@ -1,8 +1,9 @@
 function regenerateODEProblem!(mdl::PMModel)
     pp = [getproperty(mdl.parameters, x).value => PMParameterizedBase.getNumericValue(getproperty(mdl.parameters,x)) for x in mdl.parameters.names]
     pi = [getproperty(mdl._inputs, x).value => PMParameterizedBase.getNumericValue(getproperty(mdl._inputs,x)) for x in mdl._inputs.names]
-    p = vcat(pp, pi)
+    pc = [getproperty(mdl._constants,x).value =>  ModelingToolkit.getdefault(getproperty(mdl._constants,x).value) for x in mdl._constants.names]
+    p = vcat(pp, pi, pc)
 
     u0 = [getproperty(mdl.states, x).value => PMParameterizedBase.getNumericValue(getproperty(mdl.states,x)) for x in mdl.states.names]
-    mdl._odeproblem = remake(mdl._odeproblem, p = p, u0 = u0)
+    mdl._odeproblem = remake(mdl._odeproblem; p = p, u0 = u0)
 end
