fa3d6231ea014c5b4d42f318440256b2da7909bd
Timothy Knab <knabt@metrumrg.com>
2023-10-28 21:06:14 -0400

    include constants in pvector

--- src/modelingTools.jl
+++ src/modelingTools.jl
@@ -31,7 +31,7 @@ end
 
 
 function DifferentialEquations.solve(mdl::PMModel, alg::Union{DEAlgorithm,Nothing} = nothing; kwargs...)
-    p = vcat(mdl.parameters.values, mdl._inputs.values)
+    p = vcat(mdl.parameters.values, mdl.constants.values, mdl._inputs.values)
     prob = remake(mdl._odeproblem, p = p, u0 = mdl.states.values)
     sol = solve(prob, alg; kwargs...)
     solution = PMSolution(_solution = sol,
@@ -45,7 +45,7 @@ end
 
 
 function solve!(mdl::PMModel, alg::Union{DEAlgorithm,Nothing} = nothing; kwargs...)
-    p = vcat(mdl.parameters.values, mdl._inputs.values)
+    p = vcat(mdl.parameters.values, mdl.constants.values, mdl._inputs.values)
     prob = remake(mdl._odeproblem, p = p, u0 = mdl.states.values)
     sol = solve(prob, alg; kwargs...)
     solution = PMSolution(_solution = sol,
