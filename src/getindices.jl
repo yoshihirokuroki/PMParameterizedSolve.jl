b5f5b2b6731d0428dd439b1a8e124465043e0bd2
Timothy Knab <knabt@metrumrg.com>
2023-10-28 20:34:49 -0400

    Fix getindices.

--- src/getindices.jl
+++ src/getindices.jl
@@ -2,17 +2,19 @@
     sys = getfield(x,:_solution).prob.f.sys
     ivsym = ModelingToolkit.independent_variable(sys).metadata[ModelingToolkit.VariableSource][2]
     if sym in getfield(x, :_names)
-        if sym in keys(x._observed._values)
-            obs = x._observed._values[sym]._valmap[x._observed._values[sym].value]
-            out = x._solution[obs]
+        if sym in x._observed.names
+            obs = x._observed
+            idx = obs.sym_to_val[sym]
+            quantity = obs.values[idx].second
+            out = x._solution[quantity]
         else
             out = x._solution[sym]
         end
         if length(out) == 1
             out = ones(length(x._solution)) .* out
         end
-    elseif sym == ivsym
-        out = out._solution[sym]
+    elseif sym ==  ivsym
+        out = x._solution[sym]
     else
         out = getfield(x, sym)
     end
